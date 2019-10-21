ad7d5a6c3ee4a2fab440f2d055051ab6561a6814
Henrik Merlander <henrik.merlander@gmail.com>
2019-10-21 22:56:23 +0200

    Use test dummy in free vehicle tests

---
+++ TollCalculator.Domain.Tests/Dummies/TollFreeVehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TollCalculator.Domain.Vehicles;
+
+namespace TollCalculator.Domain.Tests.Dummies
+{
+    public class TollFreeVehicle : IVehicle
+    {
+        public string GetVehicleType()
+        {
+            return "Toll Free Vehicle";
+        }
+
+        public bool IsTollFree()
+        {
+            return true;
+        }
+    }
+}
--- TollCalculator.Domain.Tests/FreeVehiclesTests.cs
+++ TollCalculator.Domain.Tests/FreeVehiclesTests.cs
@@ -1,34 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TollCalculator.Domain.Tests.Dummies;
-using TollCalculator.Domain.Vehicles;
 
 namespace TollCalculator.Domain.Tests
 {
     [TestClass]
     public class FreeVehiclesTests
     {
-        [DataTestMethod]
-        [DynamicData(nameof(FeeFreeVehicles), DynamicDataSourceType.Method)]
-        public void FeeFreeVehichlesAreNotCharged(IVehicle vehicle)
+        [TestMethod]
+        public void TollFreeVehichlesAreNotCharged()
         {
             var sut = new EvolveTollCalculator(new NeverHolidayProvider(), new FixedFeeSchedule(10));
-            var date = DateTime.Parse("2019-10-04T15:00:00"); // Friday afternoon
 
-            var tollFee = sut.GetTollFee(vehicle, new DateTime[] { date });
+            var tollFee = sut.GetTollFee(new TollFreeVehicle(), new DateTime[] { DateTime.Parse("2019-10-04T15:00:00") });
 
             Assert.AreEqual(0, tollFee);
-        }
-
-        public static IEnumerable<object[]> FeeFreeVehicles()
-        {
-            yield return new object[] { new Motorbike() };
-            yield return new object[] { new Tractor() };
-            yield return new object[] { new Emergency() };
-            yield return new object[] { new Diplomat() };
-            yield return new object[] { new Foreign() };
-            yield return new object[] { new Military() };
         }
     }
 }
