89244a246320d6f4fbe9212ad531f6ff5dc64748
Henrik Merlander <henrik.merlander@gmail.com>
2019-10-21 22:56:28 +0200

    Add Nager holiday provider

---
+++ TollCalculator.Domain/Holidays/NagerHolidayProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Nager.Date;
+
+namespace TollCalculator.Domain.Holidays
+{
+    public class NagerHolidayProvider : IHolidayProvider
+    {
+        public bool IsHoliday(DateTime date)
+        {
+            return DateSystem.IsPublicHoliday(date, CountryCode.SE);
+        }
+    }
+}
