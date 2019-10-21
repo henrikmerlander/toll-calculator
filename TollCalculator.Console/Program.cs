7f8b4063147f3c5a2bff7442626eed2e0367f7d7
Henrik Merlander <henrik.merlander@gmail.com>
2019-10-21 22:56:32 +0200

    Add console example

---
+++ TollCalculator.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using TollCalculator.Domain;
+using TollCalculator.Domain.FeeSchedule;
+using TollCalculator.Domain.Holidays;
+using TollCalculator.Domain.Vehicles;
+
+namespace TollCalculator.ConsoleExample
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var holidayProvider = new NagerHolidayProvider();
+            var feeSchedule = new EvolveFeeSchedule();
+            var tollCalculator = new EvolveTollCalculator(holidayProvider, feeSchedule);
+
+            var vehicle = new Car();
+            var dates = new DateTime[]
+            {
+                DateTime.Parse("2019-10-04T05:50:00"),
+                DateTime.Parse("2019-10-04T06:10:00"),
+                DateTime.Parse("2019-10-04T12:00:00"),
+                DateTime.Parse("2019-10-04T16:50:00"),
+                DateTime.Parse("2019-10-04T17:10:00"),
+            };
+
+            Console.WriteLine($"Fee schedule:\n{feeSchedule.Print()}");
+            Console.WriteLine($"Vehicle: {vehicle.GetVehicleType()}");
+            Console.WriteLine($"Passes:\n{string.Join("\n", dates)}");
+            Console.WriteLine($"Total toll fee: {tollCalculator.GetTollFee(vehicle, dates)}");
+        }
+    }
+}
